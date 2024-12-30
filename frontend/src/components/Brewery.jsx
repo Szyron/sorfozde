46aad7f132894a6864ef2a7b37d219fb0cc5fc44
Szirony Balázs Gábor <szironybalazs@gmail.com>
2024-12-30 13:00:19 +0100

    Fix hungarian words

--- frontend/src/components/Brewery.jsx
+++ frontend/src/components/Brewery.jsx
@@ -5,12 +5,12 @@ function Brewery({ breweries }) {
     <div className="card bg-accent w-96 shadow-xl m-5">
       <div className="card-body text-neutral">
         <h2 className="card-title" >Név: {breweries.name}</h2>
-        <p>Iranyitoszam: {breweries.postal_code} </p>
-        <p>Orszag: {breweries.country}</p>
-        <p>Allam: {breweries.state} </p>
-        <p>Varos: {breweries.city}</p>
+        <p>Iranyítoszám: {breweries.postal_code} </p>
+        <p>Ország: {breweries.country}</p>
+        <p>Állam: {breweries.state} </p>
+        <p>Város: {breweries.city}</p>
         <p>Utca: {breweries.street}</p>
-        <p>Telefonszam: {breweries.phone}</p>
+        <p>Telefonszám: {breweries.phone}</p>
         <div className="card-actions justify-end">
           <button className="btn btn-error text-accent mx-auto">Weboldal</button>
         </div>
